90da675d653caa5382d55a65e38753d52e863ef8
samuelfr98 <46605132+samuelfr98@users.noreply.github.com>
2023-03-17 23:32:28 -0500

    Added bio button press

--- src/Components/Mobile/screens/AboutMobile.js
+++ src/Components/Mobile/screens/AboutMobile.js
@@ -1,27 +1,57 @@
 import React from "react";
+import { useState } from "react";
 
 const AboutMobile = () => {
+  const [openedTabs, setOpenedTabs] = useState({
+    bio: false,
+    skills: false,
+    resume: false,
+    interests: false,
+  });
+
+  const handleOpenedTab = (tab) => {
+    if (tab == "bio") {
+      setOpenedTabs({
+        bio: !openedTabs.bio,
+        skills: false,
+        resume: false,
+        interests: false,
+      });
+    }
+  };
+
   return (
     <div className="">
-      <div className="screenHeaderMobile">
-        about
-      </div>
+      <div className="screenHeaderMobile">about</div>
       <div className="screenContainerMobile">
-        <div className="screenButton" onClick={() => ''}>
+        <div className="screenButton" onClick={() => handleOpenedTab("bio")}>
           <p>bio</p>
-          {/* <img>open</img> */}
+          {openedTabs.bio ? (
+            <div className="bioMobile">
+              <div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                wtfff
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
-        <div className="screenButton" onClick={() => ''}>
+        <div className="screenButton" onClick={() => ""}>
           <p>skills</p>
         </div>
 
-        <div className="screenButton" onClick={() => ''}>
+        <div className="screenButton" onClick={() => ""}>
           <p>interests</p>
         </div>
-        <div className="screenButton" onClick={() => ''}>
+        <div className="screenButton" onClick={() => ""}>
           <p>resume</p>
         </div>
-
       </div>
     </div>
   );
